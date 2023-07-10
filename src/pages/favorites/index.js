939ec83ce4ea0295ef420e26f08fee9880adf2d0
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-07-10 19:04:36 -0400

    segundo comiit

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
 import { StyleSheet } from 'react-native';
+import { getFavorites } from '../../utils/storage';
+import { useIsFocused } from '@react-navigation/native';
 
 export function Favorites() {
+  const [receipes, setReceipes] = useState([]);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+
+    let isActive = true;
+
+    async function getReceipes() {
+      const result = await getFavorites("@appreceitas")
+      
+      if(isActive){
+        setReceipes(result)
+      }
+    }
+    
+    if(isActive){
+      getReceipes();
+    }
+  }, [isFocused])
+
   return (
-    <View style={styles.container}>
-        <Text>Favorites</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Text style= {styles.title}>Meus favoritos</Text>
+        {receipes.length === 0 && (
+          <Text>Você não tem nenhuma receita salva</Text>
+        )}
+    </SafeAreaView>
   );
 }
   const styles = StyleSheet.create({
     container:{
-        backgroundColor:'blue',
+        flex:1,
+        backgroundColor:'#f3f9ff',
+        paddingStart:14,
+        paddingEnd:14,
+        paddingTop: 36
+    },
+    title:{
+      color: "#000",
+      fontWeight:'bold',
+      fontSize: 24
     }
   })
